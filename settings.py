66277291c5878370539a44292225b021e7070124
4ndr <andrepereiraleite@gmail.com>
2018-03-06 11:00:00 -0300

    removed leaflet markercluster to commit all the marketcluster js and css files

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8^9%7kbpivdu*_pd#5hs=@1lsjap+4*%gonckdhkp%^o-j)#a1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','ec2-13-59-83-240.us-east-2.compute.amazonaws.com']
 
 
 # Application definition
